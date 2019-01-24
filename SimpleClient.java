0bd56e34330bdd57ef3c5ad82a377c1001d93a40
stalkerpnv <stalker_pnv@mail.ru>
2019-01-24 20:58:35 +0900

    Hw6 is done

---
+++ SimpleClient.java
@@ -0,0 +1,70 @@
+package ver2;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.*;
+
+public class SimpleClient {
+    final String SERVER_ADDRESS = "localhost";
+    final int SERVER_PORT = 9000;
+    Socket socket;
+    Scanner in, console;
+    PrintWriter out;
+
+    public static void main(String[] args) {
+        new SimpleClient();
+    }
+    SimpleClient(){
+        try {
+            socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        if (in.hasNext()) {
+                            String w = in.nextLine();
+                            if (w.equalsIgnoreCase("end session")) break;
+                            System.out.println(w);
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Good bye!");
+                }
+            }
+        }).start();
+        startMessaging();
+    }
+
+    //send messages method
+    public void startMessaging() {
+        String message;
+        console = new Scanner(System.in);
+        System.out.println("Enter message:");
+
+        while (true) {
+            message = console.next();
+            out.println(message);
+            out.flush();
+            if (message.equalsIgnoreCase("END")) {
+                try {
+                    console.close();
+                    out.close();
+                    in.close();
+                    socket.close();
+                    return;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
